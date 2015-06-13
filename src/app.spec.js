dec62280094ef226d8a9ea2c7546f5c0795559bd
Peter Marton <peter@risingstack.com>
2015-06-13 09:51:35 +0200

    Setup test bdd

--- src/app.spec.js
+++ src/app.spec.js
@@ -1,3 +1,12 @@
+var expect = require('chai').expect;
+
 describe('app', function () {
-  it('should initialize app');
+  it('should initialize app', function () {
+    expect(1).to.be.equal(1);
+    expect({
+      foo: 'bar'
+    }).to.be.eql({
+      foo: 'bar'
+    });
+  });
 });
